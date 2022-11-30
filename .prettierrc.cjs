4eb8aabdbc3197ef3693f605a1ced708cd44d0a2
Zachary Stence <zachstence@gmail.com>
2022-11-30 11:23:46 -0600

    fix: use cjs file for prettier config
    https://github.com/idahogurl/vs-code-prettier-eslint/issues/113#issuecomment-1327929167

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,9 @@
-{
+module.exports = {
 	"useTabs": true,
 	"singleQuote": true,
 	"trailingComma": "none",
 	"printWidth": 100,
-	"plugins": ["prettier-plugin-svelte"],
+	"plugins": [require("prettier-plugin-svelte")],
 	"pluginSearchDirs": ["."],
 	"overrides": [{ "files": "*.svelte", "options": { "parser": "svelte" } }]
 }
